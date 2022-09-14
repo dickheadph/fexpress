8a194b5cb6ad9a51d7378a50f4f91833209abc6d
shanegel <48678135+shanegel@users.noreply.github.com>
2022-09-15 01:08:00 +0800

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,23 +6,25 @@ function Card(props) {
       <div className="">
         <div className="w-auto mx-1 border-[1px] outline-none border-red-500 border-b-transparent rounded-t-xl">
           {/* {props.foodImage} */}
-          <img src={food} className="w-full h-56" alt="@" />
+          <img src={food} className="w-full lg:h-56 h-44" alt="@" />
         </div>
-        <div className="p-4 border-[1px] outline-none border-red-500 border-t-transparent rounded-b-xl mx-1 bg-gradient-to-b from-transparent to-red-50">
-          <div className="flex mb-2 lg:space-x-36 space-x-12 text-lg font-semibold">
-            <h1 className="">Burger De Lata</h1>
-            <h2 className="text-right">$ 666.00</h2>
+        <div className="px-2 border-[1px] outline-none border-red-500 border-t-transparent rounded-b-xl mx-1 bg-gradient-to-b from-transparent to-red-50">
+          <div className="lg:flex flex-wrap my-2 font-semibold text-sm">
+            <h1 className="">
+              Burger
+              <span className="lg:ml-44 ml-14">$ 666.00</span>
+            </h1>
           </div>
-          <p className="text-left pt-2">
+          <p className="text-left lg:text-base text-sm">
             Lorem ipsum et al nanit demos tado m ipsum et al nanit demos tado
             nanit demos.
           </p>
           {/* star svg here */}
           <div className="mt-3 text-center">
-            <p className="lg:inline lg:text-left mr-10 font-semibold">
-              ***** (1k+ Review)
+            <p className="lg:inline lg:text-left lg:mr-10 font-semibold">
+              *****
             </p>
-            <button className="bg-red-500 lg:py-3 lg:px-4 lg:text-lg py-2 px-3 lg:text-base font-semibold text-white rounded-full mt-4 ease-out transform hover:scale-125 transition duration-500 px-6 py-2 m-6 inline">
+            <button className="bg-red-500 lg:py-3 lg:px-4 lg:text-lg py-2 px-3 font-semibold text-white rounded-full mt-2 ease-out transform hover:scale-125 transition duration-500 mb-2 inline">
               Add to Cart
             </button>
           </div>
