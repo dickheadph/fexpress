bc5dc65e2d3c2bc13b2c53f99fb9a6622a9ed173
shanegel <48678135+shanegel@users.noreply.github.com>
2022-09-15 01:06:46 +0800

    Update OrderNow.jsx

--- src/Components/OrderNow.jsx
+++ src/Components/OrderNow.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function OrderNow(props) {
   return (
     <div>
-      <button className="ease-out transform hover:scale-125 transition duration-500 px-3 py-2 my-6 inline bg-red-500 rounded-full mt-5 font-semibold text-white text-lg">
+      <button className="ease-out transform hover:scale-125 transition duration-500 px-3 py-2 my-6 inline bg-red-500 rounded-full mt-5 font-semibold text-white lg:text-lg">
         {props.btnName}
       </button>
       <button className="inline animate-bounce ml-5 bg-red-200 lg:p-3 p-2 rounded-full">
@@ -24,7 +24,7 @@ function OrderNow(props) {
           ></path>{" "}
         </svg>
       </button>
-      <p className="inline mx-6 text-xl text-zinc-800 font-semibold">
+      <p className="inline mx-6 lg:text-xl text-md text-zinc-800 font-semibold">
         {props.btnOrder}
       </p>
     </div>
